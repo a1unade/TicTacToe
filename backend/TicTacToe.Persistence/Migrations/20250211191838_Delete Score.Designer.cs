53c5506b82885cacf1745089173e23d57347ad61
BuLbl4 <113981565+BuLbl4@users.noreply.github.com>
2025-02-11 22:19:34 +0300

    fix

--- backend/TicTacToe.Persistence/Migrations/20250211191838_Delete Score.Designer.cs
+++ backend/TicTacToe.Persistence/Migrations/20250211191838_Delete Score.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TicTacToe.Persistence.EfContext;
@@ -11,9 +12,11 @@ using TicTacToe.Persistence.EfContext;
 namespace TicTacToe.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250211191838_Delete Score")]
+    partial class DeleteScore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/TicTacToe.Persistence/Migrations/20250211191838_Delete Score.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacToe.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteScore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
